c7d362f3952adac3d1c14365d845c694e40692ac
ahmednule <nuleahmed6@gmail.com>
2025-06-04 00:01:04 +0300

    update: cleaned out the introduction abit about how i dived into wed developmenet

--- src/components/page/about-page/Introduction.tsx
+++ src/components/page/about-page/Introduction.tsx
@@ -5,15 +5,15 @@ const Introduction = () => {
     <SectionWrapper>
       <p>
         Computers have been my passion since childhood, starting with my
-        uncle&apos;s old LCD display where I learned the basics of computing.
+        cousins&apos;s old LCD display where I learned the basics of computing.
         Through his assistance and my self-driven exploration, I quickly
         mastered essential computer skills and developed a strong foundation in
         digital technology.
       </p>
       <p>
-        My programming journey began in high school, starting with HTML and CSS
-        before advancing to JavaScript. University opened new doors as I
-        embraced modern frameworks like Flutter and libraries like React,
+        My programming journey began in college, starting with C and Pascal programming lanuages
+        before advancing to JavaScript. College opened new doors as I
+        embraced modern frameworks like Django and libraries like React,
         expanding my development capabilities significantly.
       </p>
       <p>
