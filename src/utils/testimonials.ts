1a7f0da2bee203a24c6b3e2741bf3ccdee701d00
ahmednule <nuleahmed6@gmail.com>
2025-09-25 21:40:14 +0300

    update: upgated the status of the reffer

--- src/utils/testimonials.ts
+++ src/utils/testimonials.ts
@@ -3,7 +3,7 @@ export const TESTIMONIALS = [
     quote:
       "During Ahmed's time with us, he completed all assigned tasks and showed initiative to take on additional responsibilities. I found his overall performance to be good.",
     name: "Indresh Saluja",
-    designation: "Chief Business Transformation Officer at acorn",
+    designation: "Chief Operations Officer",
     src: "/assets/images/indresh.jpeg",
   },
   {
