16e28c94041e7854ef9c0917bddb3f1094e9f3ef
ahmednule <nuleahmed6@gmail.com>
2025-09-25 22:42:32 +0300

    fix: removed unused varibales for linting purposes

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,14 +4,6 @@ import { useRouter } from "next/navigation";
 import { HeroUIProvider } from "@heroui/system";
 import { NavigationProvider } from "../lib/contexts/NavigationContext";
 
-declare module "@react-types/shared" {
-  interface RouterConfig {
-    routerOptions: NonNullable<
-      Parameters<ReturnType<typeof useRouter>["push"]>[1]
-    >;
-  }
-}
-
 export function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter();
 
