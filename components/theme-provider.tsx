b982c265d9f60e2df4ebd8e3e3c3ab5f0633ebe0
ahmednule <nuleahmed6@gmail.com>
2026-04-04 17:28:49 +0300

    update: added the default background color to dark

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -9,7 +9,7 @@ import {
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem={true}>
+    <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem={false}>
       {children}
     </NextThemesProvider>
   )
